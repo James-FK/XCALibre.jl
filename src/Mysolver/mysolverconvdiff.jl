3850ccbcab8a80e5be567f76bca24767e6f4827d
James <j4m3sknight@gmail.com>
2024-11-21 13:32:09 +0000

    added runtime and tried to add boundary conditions

--- src/Mysolver/mysolverconvdiff.jl
+++ src/Mysolver/mysolverconvdiff.jl
@@ -1,13 +1,15 @@
 #testing
 using XCALibre
 grids_dir = pkgdir(XCALibre, "examples/0_GRIDS")
-grid = "trig40.unv"
+grid = "quad40.unv"
 mesh_file = joinpath(grids_dir, grid)
 mesh = UNV2D_mesh(mesh_file, scale=0.001)
+backend = CPU()
+hardware = set_hardware(backend=backend, workgroup=4)
 
 mesh_dev = mesh
 
-meshData = XCALibre.VTK.initialise_writer(mesh)
+#meshData = XCALibre.VTK.initialise_writer(mesh)
 
 
 
@@ -23,10 +25,21 @@ Divergence{Linear}(rhoU, ϕ)
 - Laplacian{Linear}(Γ, ϕ) 
 == 
 Source(ϕ_source)
-) → ScalarEquation(mesh)
+) → ScalarEquation(mesh)  
 
-schemes = (
-    ϕ = set_schemes(divergence = Linear))
+
+velocity = [1.0, 0.0, 0.0]
+
+@assign!  ϕ (
+    Dirichlet(:inlet, velocity),
+    Neumann(:outlet, 0.0),
+    Wall(:wall, [0.0, 0.0, 0.0]),
+    Wall(:top, [0.0, 0.0, 0.0]),
+)
+
+
+#schemes = (
+    #ϕ = set_schemes(divergence = Linear))
 
 
 
@@ -41,8 +54,11 @@ solvers = (
         atol = 1e10
     ))
 
+
+    runtime = set_runtime(iterations=200, time_step=1, write_interval=200)
 config = Configuration(
     solvers = solvers, schemes = schemes, runtime = runtime, hardware= hardware
 )
 
 solve_equation!(ϕ_eqn, ϕ, solvers.ϕ, config; ref=pref)
+#currently my e
