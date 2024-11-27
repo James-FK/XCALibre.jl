0022152542458aec914a3e80ec3de52b606a4730
James <j4m3sknight@gmail.com>
2024-11-27 11:31:28 +0000

    moved runtime and config to fix bugs

--- src/Mysolver/mysolverconvdiff.jl
+++ src/Mysolver/mysolverconvdiff.jl
@@ -10,9 +10,15 @@ backend = CPU()
 hardware = set_hardware(backend=backend, workgroup=4)
 
 
+#meshData = XCALibre.VTK.initialise_writer(mesh)
+
+
+
 ϕ = ScalarField(mesh)
-mdotf = FaceScalarField(mesh)
-Uf = FaceVectorField(mesh)
+rhoU = FaceScalarField(mesh)
+
+
+divrhoU = FaceScalarField(mesh)
 Γ= FaceScalarField(mesh)
 ϕ_source = ScalarField(mesh)
 
@@ -25,21 +31,18 @@ Source(ϕ_source)
 ) → ScalarEquation(mesh)  
 
 
-phi = 1.0
+velocity = 1.0
 
 
-ϕ = assign(ϕ,    Dirichlet(:inlet, phi),
+ϕ = assign(ϕ,    Dirichlet(:inlet, velocity),
 Dirichlet(:outlet, 0.0),
 Neumann(:bottom, 0.0),
 Neumann(:top, 0.0))
 #outlet should be diriclet
 
-#schemes = (
-    #ϕ = set_schemes(divergence = Linear))
+schemes = (; ϕ = set_schemes(divergence = Linear))
 Γ.values .= 0.1 
-Uf.x.values .= 1.0
-
-flux!(mdotf, Uf, config)
+rhoU.values .= 0.0
 
 
 solvers = (
@@ -52,21 +55,20 @@ solvers = (
         rtol = 1e-4,
         atol = 1e-14,
         itmax = 500
-    ),)
-@reset ϕ_eqn.preconditioner = set_preconditioner(
-                    solvers.ϕ.preconditioner, ϕ_eqn, ϕ.BCs, config)
-                    @reset ϕ_eqn.solver = solvers.ϕ.solver(_A(ϕ_eqn), _b(ϕ_eqn))
+    ),
+    )
+    
 runtime = set_runtime(iterations=200, time_step=1, write_interval=200)
-config = Configuration(
-    solvers = solvers, schemes = schemes, runtime = runtime, hardware= hardware
-)
-initialise!(ϕ, 0.0)
+config = Configuration(solvers = solvers, schemes = schemes, runtime = runtime, hardware= hardware)
+
+@reset ϕ_eqn.preconditioner = set_preconditioner(solvers.ϕ.preconditioner, ϕ_eqn, ϕ.BCs, config)
+@reset ϕ_eqn.solver = solvers.ϕ.solver(_A(ϕ_eqn), _b(ϕ_eqn))
+
+initialise!(ϕ, 1.0)
 
 
 solve_equation!(ϕ_eqn, ϕ, solvers.ϕ, config)
-#loop this a few times 
-
+#currently my e
 
 meshData = initialise_writer(mesh)
 write_vtk("test", mesh, meshData, ("phi", ϕ))
-ϕ.values
